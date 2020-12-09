61851483c1a6da672f69a9b621542af4f590a8bd
RyanSu <563607356@qq.com>
2020-12-09 23:58:07 +0800

    404页面，导航栏写å成全局组件去引入

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,9 @@
     <hr>
   <router-view></router-view>
     <hr>
-    <div>
-      <router-link to="/money">记账</router-link>|
-      <router-link to="/labels">标签</router-link>|
-      <router-link to="/statistics">统计</router-link>
 
-    </div>
   </div>
 </template>
-
 <style lang="scss">
 //@import '~@/assets/styles/test';
 //body{
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,15 @@
 <template>
-<div>Money</div>
+<div>Money
+<Nav/>
+</div>
 </template>
 
 <script lang="ts">
+
+import Nav from '@/components/Nav.vue';
 export default {
-name: "Money"
+name: "Money",
+  components: {Nav}
 }
 </script>
 
