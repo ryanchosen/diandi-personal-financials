cbabf05e961e2fb9351d6238839491e458e9bfeb
RyanSu <563607356@qq.com>
2020-12-12 19:25:24 +0800

    js制作了收入支出的切换功能

--- src/components/Types.vue
+++ src/components/Types.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type==='-'?'selected':''" @click="selectType('-')">支出</li>
+      <li :class="type==='+'?'selected':''" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 export default {
-name: "Types"
-}
+  name: 'Types',
+  data() {
+    return {
+      type: '-' //  减号支出，加号收入
+    };
+  },
+  methods:{
+    selectType(type){
+      if(type!=='-'&&type!=='+'){
+        throw new Error('type is unknown') // 健壮性
+      }
+      this.type =type;
+
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
