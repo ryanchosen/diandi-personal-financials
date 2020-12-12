fae1713819661423f3852ce6d8627288cc8b1b14
RyanSu <563607356@qq.com>
2020-12-12 17:57:11 +0800

    重新思考了一下flex布局的思路

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,32 +1,28 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad/>
-    <Types/>
+ <Tags/>
     <Notes/>
-    <Tags/>
+    <Types/>
+    <NumberPad/>
   </Layout>
 </template>
 
 <script lang="ts">
 
-import NumberPad from '@/components/NumberPad.vue';
-import Types from '@/components/Types.vue';
-import Notes from '@/components/Notes.vue';
 import Tags from '@/components/Tags.vue';
-
+import Notes from '@/components/Notes.vue';
+import Types from '@/components/Types.vue';
+import NumberPad from '@/components/NumberPad.vue';
 export default {
   name: 'Money',
-  components: {Tags, Notes, Types, NumberPad},
+  components: {NumberPad, Types, Notes, Tags},
 };
 </script>
 <style lang="scss">
-.layout-content {
-  border: 1px solid red;
-  display: flex;
-  flex-direction: column-reverse;
+.layout-content{
+  border:1px solid red;
+  display:flex;
+  flex-direction: column;
 }
 </style>
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
