34df296b4cf07eab75759e620798a57bbbfb83fc
RyanSu <563607356@qq.com>
2020-12-12 16:52:52 +0800

    模块化:把money页面拆成几个小的组件，不用但传值问题！

---
+++ src/components/Types.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">支出</li>
+      <li>收入</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+name: "Types"
+}
+</script>
+
+<style lang="scss" scoped>
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+
+  li {
+    width: 50%;
+    line-height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &.selected {
+      position: relative;
+
+      &::after {
+        content: '';
+        position: absolute;
+        height: 4px;
+        background: #333333;
+        width: 100%;
+        bottom: 0;
+        display: block;
+      }
+
+    }
+
+  }
+}
+</style>
